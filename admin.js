f973cda067955ba1d1ec4bb262981b8d9bdcd486
limberruthcamila <limberhuaychoquispe81@gmail.com>
2026-04-08 15:31:46 -0400

    Add files via upload

--- admin.js
+++ admin.js
@@ -3,11 +3,11 @@
 const firebaseConfig = {
   apiKey: "AIzaSyBG_DjQQfroIKY3Fta2PgsfE9ArRYfqrh0",
   authDomain: "project-3081-limber.firebaseapp.com",
+  databaseURL: "https://project-3081-limber-default-rtdb.firebaseio.com",
   projectId: "project-3081-limber",
   storageBucket: "project-3081-limber.firebasestorage.app",
   messagingSenderId: "174564084979",
-  appId: "1:174564084979:web:d141d7ee968595f1d4bb86",
-  measurementId: "G-K99Q5PX8W2"
+  appId: "1:174564084979:web:d141d7ee968595f1d4bb86"
 };
 
 if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
--- app.js
+++ app.js
@@ -4,12 +4,13 @@
 const firebaseConfig = {
   apiKey: "AIzaSyBG_DjQQfroIKY3Fta2PgsfE9ArRYfqrh0",
   authDomain: "project-3081-limber.firebaseapp.com",
+  databaseURL: "https://project-3081-limber-default-rtdb.firebaseio.com",
   projectId: "project-3081-limber",
   storageBucket: "project-3081-limber.firebasestorage.app",
   messagingSenderId: "174564084979",
-  appId: "1:174564084979:web:d141d7ee968595f1d4bb86",
-  measurementId: "G-K99Q5PX8W2"
+  appId: "1:174564084979:web:d141d7ee968595f1d4bb86"
 };
+
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.database();
